b1af6941242cb715e3ff0c4a3a390e869361f482
John Cipriano <johnmikecip@gmail.com>
2020-03-04 00:56:03 -0500

    refactor flyer.rb

--- flyer.rb
+++ flyer.rb
@@ -5,16 +5,17 @@ require 'date'
 require 'fileutils'
 require 'launchy'
 
+
+HTML_OUT = File.join('output', 'index.html').freeze
+
+
+# An inventory item / sold product.
 Item = Struct.new(:id, :description, :category, :price)
 
-def date_of_next(day)
-  date  = Date.parse(day)
-  delta = date > Date.today ? 0 : 7
-  date + delta
-end
 
+# TODO: make this generic again
 def read_table(table_name)
-  ODBC::connect('','','') do |db|
+  ODBC.connect('', '', '') do |db|
     rows = db.run("select * from #{table_name}").each_hash
 
     rows.map do |row|
@@ -28,44 +29,78 @@ def read_table(table_name)
   end
 end
 
-def main
-  # load items
-  @all_items = read_table('SRCFILE')
+# Reads a list of included item numbers from a file, and returns a filtered
+# list of Item objects.
+def fetch_produce_items
+  all_items = read_table('SRCFILE')
+  included_ids = File.readlines(File.join('data', 'included.txt')).map(&:chomp)
 
-  # filter items
-  @included_ids = File.readlines(File.join('data', 'included.txt')).map(&:chomp)
-  @items = @all_items.select { |i| @included_ids.include?(i.id) }
+  @items = all_items.select do |i|
+    included_ids.include?(i.id)
+  end
+end
 
-  # fill prices
-  @prices = CSV.readlines(File.join('data', 'prices.csv')).to_h
-  @prices.transform_values! do |val|
+# Sets the price attribute for each Item object, after reading the
+# prices from a file.
+def set_prices
+  prices = CSV.readlines(File.join('data', 'prices.csv')).to_h
+  prices.transform_values! do |val|
     val.strip.to_f
   end
-  @items.each { |i| i.price = @prices[i.id] }
 
-  # fill categories
-  @categories = CSV.readlines(File.join('data', 'categories.csv')).each_with_object({}) do |(key, val), hsh|
+  @items.each do |i|
+    i.price = prices[i.id]
+  end
+end
+
+# Sets the category attribute for each Item object, after reading the
+# category names from a file.
+def set_categories
+  csv = CSV.readlines(File.join('data', 'categories.csv'))
+  @categories = csv.each_with_object({}) do |(key, val), hsh|
     hsh[key] = val
   end
-  @items.each { |i| i.category = @categories[i.category] }
+
+  @items.each do |i|
+    i.category = @categories[i.category]
+  end
+end
+
+# Copies static files needed for site to the output dir.
+def copy_assets
+  FileUtils.cd('view') do
+    %w[style.css napoli.png ferraro.svg].each do |name|
+      FileUtils.cp(name, File.join('..', 'output', name))
+    end
+  end
+end
+
+# Returns the date of the next day of the week, given the name of the day.
+def date_of_next(day)
+  date  = Date.parse(day)
+  delta = date > Date.today ? 0 : 7
+  date + delta
+end
+
+# Returns the slim template.
+def template
+  Slim::Template.new(File.join('view', 'index.html.slim'))
+end
+
+# Makes the index.html file.
+def build_html
+  fetch_produce_items
+  set_prices
+  set_categories
 
   @monday = date_of_next('Monday')
   @grouped_items = @items.group_by(&:category)
 
-  # make html
-  template = Slim::Template.new(File.join('view', 'index.html.slim'))
-  outfile_name = File.join('output', 'index.html')
-  File.open(outfile_name, 'wb') do |file|
-    file << template.render(self)
+  File.open(HTML_OUT, 'wb') do |f|
+    f << template.render(self)
   end
-  template.render(self)
-
-  # copy stylesheet
-  FileUtils.cp(File.join('view', 'style.css'), File.join('output', 'style.css'))
-  FileUtils.cp(File.join('view', 'napoli.png'), File.join('output', 'napoli.png'))
-  FileUtils.cp(File.join('view', 'ferraro.svg'), File.join('output', 'ferraro.svg'))
-
-  Launchy.open(outfile_name)
 end
 
-main
+build_html
+copy_assets
+Launchy.open(HTML_OUT)
